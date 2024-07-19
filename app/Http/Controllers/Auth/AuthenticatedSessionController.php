3ee5185e58e27dc8e7568a7cae6758349c268eef
dantechdevs <damnngwasi@gmail.com>
2024-07-19 09:04:19 +0300

    updated

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,17 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        $url = '';
+        if($request->user()->role === 'admin'){
+            $url = 'admin/dashboard' ;
+        } elseif($request->user()->role === 'office'){
+            $url = 'office/dashboard' ;
+
+        } elseif($request->user()->role === 'user'){
+            $url = '/dashboard' ;
+        }
+
+        return redirect()->intended($url);
     }
 
     /**
