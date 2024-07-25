3578ffb33804b8369386039101e0fe4929eea772
dantechdevs <damnngwasi@gmail.com>
2024-07-25 23:04:58 +0300

    updated

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+//use App\Models\User;
+
 
 class AdminController extends Controller
 {
@@ -11,9 +14,19 @@ class AdminController extends Controller
         return view('admin.index');
 
     } // End Method
+    public function AdminLogout(Request $request){
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    } // End method
+
 
     public function AdminLogin()
     {
-        return view('admin.admin.login');
+        return view('admin.admin_login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 Route::get('/admin/dashboard',[AdminController::class,'AdminDashboard'])->name('admin.dashboard');
-
+Route::get('/admin/logout', [AdminController::class,'AdminLogout'])->name('admin.logout');
 // office
 Route::get('/office/dashboard',[OfficeController::class,'OfficeDashboard'])->name('office.dashboard');
 // End  group office middleware
